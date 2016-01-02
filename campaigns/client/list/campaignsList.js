e6c6d6278b5457597f6896012df41cacd4f28a8f
ncphillips <ncphillips.19@gmail.com>
2016-01-02 10:34:10 -0400

    Fixed a routing bug in the campaign list.

--- campaigns/client/list/campaignsList.js
+++ campaigns/client/list/campaignsList.js
@@ -1,6 +1,5 @@
 Template.campaignsList.events({
     "click .campaign-row": function(){
-        console.log("asdf");
-        Router.go("campaignsView", {id: this._id});
+        Router.go("campaignsView", {campaignId: this._id});
     }
 });
